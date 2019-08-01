8d907d3433ed3574d345d1eb8cdd794359bd1926
SandeepDevrari <sandeepdevrari77@gmail.com>
2019-08-01 23:31:43 +0530

    Data binding used. Bug- text view not updating while pressing any of the button

--- app/src/main/java/devrari/sandeep/court_counter/MainActivity.java
+++ app/src/main/java/devrari/sandeep/court_counter/MainActivity.java
@@ -1,101 +1,106 @@
 package devrari.sandeep.court_counter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import devrari.sandeep.court_counter.databinding.ActivityMainBinding;
 import devrari.sandeep.court_counter.viewmodel.ViewModelMainActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView teamAScore,teamBScore;
+    //private TextView teamAScore,teamBScore;
     private ViewModelMainActivity viewModelMainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setUpUI();
-    }
-
-    private void setUpUI() {
         viewModelMainActivity= ViewModelProviders.of(this).get(ViewModelMainActivity.class);
-        teamAScore=findViewById(R.id.tv_score);
-        teamBScore=findViewById(R.id.tv_score_b);
-        setData(1,viewModelMainActivity.getScoreATeam());
-        setData(2,viewModelMainActivity.getScoreBTeam());
-
-        findViewById(R.id.b_2_point).setOnClickListener(this);
-        findViewById(R.id.b_3_point).setOnClickListener(this);
-        findViewById(R.id.b_free_throw).setOnClickListener(this);
-        findViewById(R.id.b_2_point_b).setOnClickListener(this);
-        findViewById(R.id.b_3_point_b).setOnClickListener(this);
-        findViewById(R.id.b_free_throw_b).setOnClickListener(this);
-        findViewById(R.id.b_reset).setOnClickListener(this);
+        ActivityMainBinding activityMainBinding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        activityMainBinding.setViewmodel(viewModelMainActivity);
+//        setContentView(R.layout.activity_main);
+//        setUpUI();
     }
 
-    private void setData(int team, int score) {
-        if(team==0){
-            teamAScore.setText(score+"");
-            teamBScore.setText(score+"");
-        }else if(team==1){
-            teamAScore.setText(score+"");
-        }else if(team==2){
-            teamBScore.setText(score+"");
-        }
-    }
+//    private void setUpUI() {
+//        viewModelMainActivity= ViewModelProviders.of(this).get(ViewModelMainActivity.class);
+//        teamAScore=findViewById(R.id.tv_score);
+//        teamBScore=findViewById(R.id.tv_score_b);
+//        setData(1,viewModelMainActivity.getScoreATeam());
+//        setData(2,viewModelMainActivity.getScoreBTeam());
+//
+//        findViewById(R.id.b_2_point).setOnClickListener(this);
+//        findViewById(R.id.b_3_point).setOnClickListener(this);
+//        findViewById(R.id.b_free_throw).setOnClickListener(this);
+//        findViewById(R.id.b_2_point_b).setOnClickListener(this);
+//        findViewById(R.id.b_3_point_b).setOnClickListener(this);
+//        findViewById(R.id.b_free_throw_b).setOnClickListener(this);
+//        findViewById(R.id.b_reset).setOnClickListener(this);
+//    }
+//
+//    private void setData(int team, int score) {
+//        if(team==0){
+//            teamAScore.setText(score+"");
+//            teamBScore.setText(score+"");
+//        }else if(team==1){
+//            teamAScore.setText(score+"");
+//        }else if(team==2){
+//            teamBScore.setText(score+"");
+//        }
+//    }
 
     @Override
     public void onClick(View view) {
-        int team=-1,score=0;
-        switch (view.getId()){
-            case R.id.b_2_point:{
-                team=1;
-                score=2+viewModelMainActivity.getScoreATeam();
-                viewModelMainActivity.setScoreATeam(score);
-                break;
-            }
-            case R.id.b_3_point:{
-                team=1;
-                score=3+viewModelMainActivity.getScoreATeam();
-                viewModelMainActivity.setScoreATeam(score);
-                break;
-            }
-            case R.id.b_2_point_b:{
-                team=2;
-                score=2+viewModelMainActivity.getScoreBTeam();
-                viewModelMainActivity.setScoreBTeam(score);
-                break;
-            }
-            case R.id.b_3_point_b:{
-                team=2;
-                score=3+viewModelMainActivity.getScoreBTeam();
-                viewModelMainActivity.setScoreBTeam(score);
-                break;
-            }
-            case R.id.b_free_throw:{
-                team=1;
-                score=5+viewModelMainActivity.getScoreATeam();
-                viewModelMainActivity.setScoreATeam(score);
-                break;
-            }
-            case R.id.b_free_throw_b:{
-                team=2;
-                score=6+viewModelMainActivity.getScoreBTeam();
-                viewModelMainActivity.setScoreBTeam(score);
-                break;
-            }
-            case R.id.b_reset:{
-                team=0;
-                score=0;
-                viewModelMainActivity.setScoreATeam(0);
-                viewModelMainActivity.setScoreBTeam(0);
-                break;
-            }
-        }
-        setData(team,score);
+//        int team=-1,score=0;
+//        switch (view.getId()){
+//            case R.id.b_2_point:{
+//                team=1;
+//                score=2+viewModelMainActivity.getScoreATeam();
+//                viewModelMainActivity.setScoreATeam(score);
+//                break;
+//            }
+//            case R.id.b_3_point:{
+//                team=1;
+//                score=3+viewModelMainActivity.getScoreATeam();
+//                viewModelMainActivity.setScoreATeam(score);
+//                break;
+//            }
+//            case R.id.b_2_point_b:{
+//                team=2;
+//                score=2+viewModelMainActivity.getScoreBTeam();
+//                viewModelMainActivity.setScoreBTeam(score);
+//                break;
+//            }
+//            case R.id.b_3_point_b:{
+//                team=2;
+//                score=3+viewModelMainActivity.getScoreBTeam();
+//                viewModelMainActivity.setScoreBTeam(score);
+//                break;
+//            }
+//            case R.id.b_free_throw:{
+//                team=1;
+//                score=5+viewModelMainActivity.getScoreATeam();
+//                viewModelMainActivity.setScoreATeam(score);
+//                break;
+//            }
+//            case R.id.b_free_throw_b:{
+//                team=2;
+//                score=6+viewModelMainActivity.getScoreBTeam();
+//                viewModelMainActivity.setScoreBTeam(score);
+//                break;
+//            }
+//            case R.id.b_reset:{
+//                team=0;
+//                score=0;
+//                viewModelMainActivity.setScoreATeam(0);
+//                viewModelMainActivity.setScoreBTeam(0);
+//                break;
+//            }
+//        }
+//        setData(team,score);
     }
 }
--- app/src/main/java/devrari/sandeep/court_counter/viewmodel/ViewModelMainActivity.java
+++ app/src/main/java/devrari/sandeep/court_counter/viewmodel/ViewModelMainActivity.java
@@ -1,6 +1,10 @@
 package devrari.sandeep.court_counter.viewmodel;
 
+import android.view.View;
+
 import androidx.lifecycle.ViewModel;
+
+import devrari.sandeep.court_counter.R;
 
 public class ViewModelMainActivity extends ViewModel {
     private int scoreATeam=0;
@@ -20,5 +24,40 @@ public class ViewModelMainActivity extends ViewModel {
 
     public void setScoreBTeam(int scoreBTeam) {
         this.scoreBTeam = scoreBTeam;
+    }
+
+    public void onButtonClick(View view){
+        //int team=-1,score=0;
+        switch (view.getId()){
+            case R.id.b_2_point:{
+                scoreATeam+=2;
+                break;
+            }
+            case R.id.b_3_point:{
+                scoreATeam+=3;
+                break;
+            }
+            case R.id.b_2_point_b:{
+                scoreBTeam+=2;
+                break;
+            }
+            case R.id.b_3_point_b:{
+                scoreBTeam+=3;
+                break;
+            }
+            case R.id.b_free_throw:{
+                scoreATeam+=5;
+                break;
+            }
+            case R.id.b_free_throw_b:{
+                scoreBTeam+=6;
+                break;
+            }
+            case R.id.b_reset:{
+                scoreBTeam=0;
+                scoreATeam=0;
+                break;
+            }
+        }
     }
 }
